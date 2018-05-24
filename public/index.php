93cee00ef9c20af784f7faabdf30efcace390eaf
romainLoire <romain.loire@ynov.com>
2018-05-24 15:15:44 +0200

    htacces

---
+++ public/index.php
@@ -0,0 +1,11 @@
+
+
+            <?php
+
+
+            if(strstr($_SERVER["REQUEST_URI"], "/stage") || strstr($_SERVER["REQUEST_URI"], "liste") || strstr($_SERVER["REQUEST_URI"], "offres"))
+              {
+                  require '..\private\src\php\list.php';
+            }
+            require '..\private\src\php\list.php';
+            ?>
