460ac7253adcd62b9fe2e33d03b4237d9777f71d
Louise <louise.baulan@ynov.com>
2018-05-25 12:31:50 +0200

    HTML 2

--- private/src/php/list.php
+++ private/src/php/list.php
@@ -5,24 +5,33 @@
         <title>
           Stage Campus Ynov - Lyon
         </title>
+        <?php
+        include 'index.css'
+        ?>
+
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <!---<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+---->
     </head>
     <?php ?>
     <body>
       <h1> Affinez votre recherche </h1>
       <h1> pour trouvez le profil qui vous correspond </h1>
-      <div id= "formulaire">
+      <div id= "form">
         <h3> Sélectionnez vos critères de recherche </h3>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="selectClass1">
-          <label class="form-check-label" for="selectClass1">
-            Bachelor 1
-          </label>
-          <input class="form-check-input" type="checkbox" value="" id="selectClass2">
-          <label class="form-check-label" for="selectClass2">
-            Bachelor 2
-          </label>
+          <label for="form-check"> Année </label>
+          <div id="years">
+            <input class="form-check-input" type="checkbox" value="" id="selectClass1">
+            <label class="form-check-label" for="selectClass1">
+              Bachelor 1
+            </label>
+            <input class="form-check-input" type="checkbox" value="" id="selectClass2">
+            <label class="form-check-label" for="selectClass2">
+              Bachelor 2
+            </label>
+          </div>
         </div>
         <div class="form-group col-md-4">
           <label for="inputThem">Thématique</label>
@@ -46,6 +55,22 @@
             <option>Node JS</option>
           </select>
         </div>
+        <button type="submit" class="btn btn-primary mb-2"> Recherche </button>
+      </div>
+
+      <div class="offer">
+         <div class="title">
+           <h4>Titre de l'offre</h4>
+         </div>
+         <div class="classe">
+           B1 Ingésup
+         </div>
+         <div class="description">
+           Ce profil bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+         </div>
+         <div class="skills">
+           Php ...
+         </div>
       </div>
     </body>
 </html>
