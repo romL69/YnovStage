1b965ab34c99f4cf828984f8a465bdff13f10d0b
Louise <louise.baulan@ynov.com>
2018-05-28 15:10:18 +0200

    assets

--- private/src/php/list.php
+++ private/src/php/list.php
@@ -28,9 +28,9 @@ if (isset($_GET['recherche']))
         <title>
           Stage Campus Ynov - Lyon
         </title>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-
-        <link rel="stylesheet" href="/YnovStage/public/master.css" >
+        <!---<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+---->
+        <link rel="stylesheet" href="/assets/master.css" >
         <!---<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 ---->
     </head>
