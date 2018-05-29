a4de4ba87e94bbacaab934689c6055ace02f4486
Louise <louise.baulan@ynov.com>
2018-05-29 16:06:26 +0200

    Offer.php + css

--- private/src/php/offer.php
+++ private/src/php/offer.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="/assets/master.css" >
+        <link rel="stylesheet" href="master.css" >
         <title>Détails de l'offre</title>
     </head>
     <body>
@@ -22,72 +22,49 @@
       </div>
 
       <div class="contact">
-        <section class="be000-column be000-column--1-column">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="be000-column__text">
-                            <div class="frm_forms " id="frm_form_2_container" ><form enctype="multipart/form-data" method="post" class="frm-show-form  frm_pro_form " id="form_contact-form"  >
-                              <div class="frm_form_fields "><fieldset>
-                                <legend class="frm_hidden">Nous contacter</legend>
-                                <input type="hidden" name="frm_action" value="create" />
-                                <input type="hidden" name="form_id" value="2" />
-                                <input type="hidden" name="frm_hide_fields_2" id="frm_hide_fields_2" value="" />
-                                <input type="hidden" name="form_key" value="contact-form" />
-                                <input type="hidden" name="item_meta[0]" value="" />
-                                <input type="hidden" id="frm_submit_entry_2" name="frm_submit_entry_2" value="346e32339b" />
-                                <input type="hidden" name="_wp_http_referer" value="/fr/nous-contacter/" />
-                                <input type="text" class="frm_hidden frm_verify" id="frm_verify_2" name="frm_verify" value=""  />
-                                <div id="frm_field_71_container" class="frm_form_field form-field  frm_required_field frm_top_container input--50">
-                                  <label for="field_q7peo" class="frm_primary_label">CIVILITE<span class="frm_required">*</span></label>
-                                  <select name="item_meta[71]" id="field_q7peo"  data-frmval="Monsieur" data-reqmsg="Ce champ ne peut pas être vide"  >
-                                    <option value="Monsieur"  selected="selected">Monsieur</option>
-                                    <option value="Madame" >Madame</option>
-                                  </select>
-                                </div>
-                                <div id="frm_field_8_container" class="frm_form_field form-field  frm_required_field frm_top_container"><label for="field_qh4icy2" class="frm_primary_label">NOM<span class="frm_required">*</span></label>
-                                  <input type="text" id="field_qh4icy2" name="item_meta[8]" value=""  data-reqmsg="Ce champ ne peut pas être vide"  />
-                                </div>
-                                <div id="frm_field_9_container" class="frm_form_field form-field  frm_required_field frm_top_container">
-                                  <label for="field_ocfup12" class="frm_primary_label">PRENOM<span class="frm_required">*</span></label>
-                                  <input type="text" id="field_ocfup12" name="item_meta[9]" value=""  data-reqmsg="Ce champ ne peut pas être vide"  />
-                                </div>
-                                <div id="frm_field_10_container" class="frm_form_field form-field  frm_required_field frm_top_container frm_full">
-                                  <label for="field_29yf4d2" class="frm_primary_label">EMAIL<span class="frm_required">*</span></label>
-                                  <input type="email" id="field_29yf4d2" name="item_meta[10]" value=""  data-reqmsg="Ce champ ne peut pas être vide" data-invmsg="Merci de renseigner un mail valide"  />
-                                </div>
-                                <div id="frm_field_11_container" class="frm_form_field form-field  frm_required_field frm_top_container">
-                                  <label for="field_itt5me2" class="frm_primary_label">TEL<span class="frm_required">*</span></label>
-                                  <input type="tel" id="field_itt5me2" name="item_meta[11]" value=""  pattern="((\+\d{1,3}(-|.| )?\(?\d\)?(-| |.)?\d{1,5})|(\(?\d{2,6}\)?))(-|.| )?(\d{3,4})(-|.| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" data-reqmsg="Ce champ ne peut pas être vide" data-invmsg="Please enter a valid website"  />
-                                </div>
-                                <div id="frm_field_72_container" class="frm_form_field form-field  frm_required_field frm_top_container">
-                                  <label for="field_a3j1p" class="frm_primary_label">FORMATION QUI M'INTERESSE<span class="frm_required">*</span></label>
-                                  <select name="item_meta[72]" id="field_a3j1p"  data-reqmsg="Ce champ ne peut pas être vide"  >
-                                    <option value=""  selected="selected"> </option>
-                                    <option value="Ynov Informatique Ingesup" >Ynov Informatique Ingesup</option>
-                                    <option value="Ynov Animation 3D" >Ynov Animation 3D</option>
-                                    <option value="Ynov Web, com et graphic design" >Ynov Web, com et graphic design</option>
-                                    <option value="Ynov Digital Business School" >Ynov Digital Business School</option>
-                                    <option value="ISEE bachelor 3" >ISEE bachelor 3</option>
-                                    <option value="Ynov Audiovisuel" >Ynov Audiovisuel</option>
-                                  </select>
-                                </div>
-                                <div id="frm_field_94_container" class="frm_form_field form-field  frm_required_field frm_top_container">
-                                  <label for="field_8pgw3" class="frm_primary_label">Dites-nous tout :<span class="frm_required">*</span></label>
-                                  <textarea name="item_meta[94]" id="field_8pgw3" rows="5"  data-reqmsg="Ce champ ne peut pas être vide"  ></textarea>
-                                </div>
-                                <input type="hidden" name="item_key" value="" />
-                                <div class="frm_submit">
-                                  <button class="frm_button_submit frm_final_submit" type="submit"   formnovalidate="formnovalidate">Envoyer</button>
-                                </div>
-                              </fieldset>
-                            </div>
-                        </div>
-                      </div>
-                    </div>
-                </div>
+            <div class="form_fields">
+              <legend class="title_contact">Nous contacter à propos de ce profil</legend>
+              <!----------<input type="hidden" name="frm_action" value="create" />
+              <input type="hidden" name="form_id" value="2" />
+              <input type="hidden" name="frm_hide_fields_2" id="frm_hide_fields_2" value="" />
+              <input type="hidden" name="form_key" value="contact-form" />
+              <input type="hidden" name="item_meta[0]" value="" />
+              <input type="hidden" id="frm_submit_entry_2" name="frm_submit_entry_2" value="346e32339b" />
+              <input type="hidden" name="_wp_http_referer" value="/fr/nous-contacter/" />
+              <input type="text" class="frm_hidden frm_verify" id="frm_verify_2" name="frm_verify" value=""  />------->
+              <div class="civility">
+                <label for="frm_civility">Civilité<span class="needed">*</span></label>
+                <select name="frm_civility" data-frmval="Monsieur" data-reqmsg="Ce champ ne peut pas être vide">
+                  <option value="Monsieur"  selected="selected">Monsieur</option>
+                  <option value="Madame" >Madame</option>
+                </select>
+              </div>
+              <div class="name">
+                <label for="frm_name">NOM<span class="frm_required">*</span></label>
+                <input type="text" name="frm_name" value=""  data-reqmsg="Ce champ ne peut pas être vide"  />
+              </div>
+              <div class="surname">
+                <label for="frm_surname">Prénom<span class="frm_required">*</span></label>
+                <input type="text" name="frm_surname" value=""  data-reqmsg="Ce champ ne peut pas être vide"  />
+              </div>
+              <div class="enterprise">
+                <label for="frm_enterprise">Nom de l'entreprise<span class="frm_required">*</span></label>
+                <input type="text" name="frm_enterprise" value=""  data-reqmsg="Ce champ ne peut pas être vide"  />
+              </div>
+              <div class="email">
+                <label for="frm_mail">E-Mail<span class="frm_required">*</span></label>
+                <input type="email" name="frm_mail" value=""  data-reqmsg="Ce champ ne peut pas être vide"  />
+              </div>
+              <div class="tel">
+                <label for="frm_mobile">Téléphone<span class="frm_required">*</span></label>
+                <input type="tel" name="frm_mobile" value=""  data-reqmsg="Ce champ ne peut pas être vide"  />
+              </div>
+              <div class"message">
+                <label for="frm_message">Dites-nous tout :<span class="frm_required">*</span></label>
+                <textarea name="frm_message" data-reqmsg="Ce champ ne peut pas être vide"></textarea>
+              </div>
+              <button type="submit" class="contact_validate"> Envoyer </button>
             </div>
-        </section>
       </div>
     </body>
 </html>
