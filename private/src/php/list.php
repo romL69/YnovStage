be18e79d1e021c205b3b2164a7a830f515f3f04c
Louise <louise.baulan@ynov.com>
2018-05-30 14:25:12 +0200

    without head

--- private/src/php/list.php
+++ private/src/php/list.php
@@ -111,7 +111,6 @@ if (isset($_GET['recherche']))
                 </select>
               </div>
               <button type="submit" class="form_submit">Recherche</button>
-=======
           <div class="form_class">
             <label for="inputClass"> Ecole </label>
             <select id="inputClass" class="form_class" name="class">
@@ -157,7 +156,6 @@ if (isset($_GET['recherche']))
             </select>
           </div>
           <button type="submit" class="form_submit">Recherche</button>
->>>>>>> 3ebc171923f938302e1ab3bad45a0a64bbe6b1ba
         </form>
       </div>
 
